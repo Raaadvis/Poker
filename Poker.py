39ca684de34850252a1442ae519475b59c355f41
Raaadvis <57595072+Raaadvis@users.noreply.github.com>
2019-11-11 22:13:41 +0200

    Poker

--- Poker.py
+++ Poker.py
@@ -4,7 +4,9 @@ import random
 deck = ["2♦", "3♦", "4♦", "5♦", "6♦", "7♦", "8♦", "9♦", "10♦", "J♦", "Q♦", "K♦", "A♦", "2♣", "3♣", "4♣", "5♣", "6♣", "7♣", "8♣", "9♣", "10♣", "J♣", "Q♣", "K♣", "A♣", "2♥", "3♥", "4♥", "5♥", "6♥", "7♥", "8♥", "9♥", "10♥", "J♥", "Q♥", "K♥", "A♥", "2♠", "3♠", "4♠", "5♠", "6♠", "7♠", "8♠", "9♠", "10♠", "J♠", "Q♠", "K♠", "A♠"]
 random.shuffle(deck)
 
-name = input("Zaidejo vardas? ")
+name = input("Player's name? ")
+Cash_player = 10000
+Cash_master = 10000
 myhand = []
 Master = []
 Flop = []
@@ -20,8 +22,18 @@ for turnrivercards in range(1):
   Turn.append(deck.pop())
   River.append(deck.pop())
 
-print(name,"is holding:",myhand)
-print("Master is holding:",Master)
-print("Flop: ",Flop)
-print("Turn: ",Turn)
-print("River: ",River)
+print("\n*****************************************************\n")
+print(f"{name} is holding: {myhand} \nCash left: {Cash_player}$")
+print("\n____________________________________________________________\n")
+
+Choice1 = input("Choose your action: Bet/Check/Fold - ")
+if Choice1 == "Check":
+    print("Flop:", Flop)
+if Choice1 == "Bet":
+    amount = int(input("Amount - "))
+    if amount >= Cash_player:
+        print("You can\'t bet more than you have, stupid face.")
+    if amount <= Cash_player:
+        Cash_left = Cash_player - amount 
+        print("Cash left - ", Cash_left)
+#print("Flop:", Flop, "Turn:", Turn, "River:", River)
