06eb164132680191b4b87a78e97b7d2a04cb76b6
Raaadvis <57595072+Raaadvis@users.noreply.github.com>
2019-11-14 17:08:06 +0200

    Update Poker.py

--- Poker.py
+++ Poker.py
@@ -21,7 +21,7 @@ Star = "*"*50
 Line = "_"*50
 Space = " "
 
-print("Texas hold \'em poker - Heads up challenge\n")
+print("Texas hold \'em poker - Heads up challenge!\n")
 name = input("Player's name? ")
 
 #Inserting cards to variables
@@ -35,42 +35,40 @@ for cards3 in range(3):
   Flop.append(deck.pop())
 
 #Gameplay formula
-#def poker_game(Cash_player = 10000, Cash_master = 10000, Pot = 0):
 
 Table1 = (f"{Newline*10}{Line}\nMaster's cash: {Cash_master}$\n\n\nPot: {Pot}{Space*10}")
-Table2 = (f"\n\n\n{name}\'s hand: {myhand} \nCash: {Cash_player}$\n{Star}\n")
+Table2 = (f"\n\n\n{name} hand: {myhand} \nCash: {Cash_player}$\n{Star}\n")
 BasicTable = (f"{Table1}{Table2}")
 FlopTable = (f"{Table1}{Flop}{Table2}")
 TurnTable = (f"{Table1}{Flop} {Turn}{Table2}")
 RiverTable = (f"{Table1}{Flop} {Turn} {River}{Table2}")
 
-Gamestep = BasicTable, FlopTable, TurnTable, RiverTable
-for gamephase in range(len(Gamestep)):
-  print(Gamestep[gamephase])
-  Choice = input("Choose your action: Bet/Check/Fold - ")
-  while Choice != "Check" and Choice != "Bet" and Choice != "Fold":
-    Choice = input("Incorrect entry, try again: ")
-  if Choice == "Check":
-    print(Line)
-    continue
-  elif Choice == "Bet":
-    betamount = int(input("Amount - "))
-    print(Line)
-    while betamount > Cash_player:
-      betamount = int(input("Incorrect amount, try again: "))
-      if betamount == 0:
-        continue
-    #Cash_player = Cash_player - betamount
-    #Cash_master = Cash_master - betamount
-    #Pot = betamount + betamount
-    def betting(betamount):
-      return betamount + betamount
-    Pot = betting(betamount)
-    print(Cash_player)
-    print(Pot)
-    continue
-  elif Choice == "Fold" or gamephase == 3:
-    print(Line, RiverTable)
-    break
-print(f"\nMaster\'s hand: {Master}\n{Line}")
-#poker_game()
+while not Cash_master == 0 or Cash_player ==0:
+  Gamestep = BasicTable, FlopTable, TurnTable, RiverTable
+  for gamephase in range(len(Gamestep)):
+    print(Gamestep[gamephase])
+    Choice = input("Choose your action: Bet/Check/Fold - ")
+    while Choice != "Check" and Choice != "Bet" and Choice != "Fold":
+      Choice = input("Incorrect entry, try again: ")
+    if Choice == "Check":
+      print(Line)
+      continue
+    elif Choice == "Bet":
+      betamount = int(input("Amount - "))
+      print(Line)
+      while betamount > Cash_player:
+        betamount = int(input("Incorrect amount, try again: "))
+        if betamount == 0:
+          continue
+      Pot += betamount*2
+      Cash_player -= betamount
+      Cash_master -= betamount
+      print(f"Cash {name}: ", Cash_player)
+      print(Pot)
+      print("Cash Master: ", Cash_master)
+      continue
+    elif Choice == "Fold" or gamephase == 3:
+      print(Line, RiverTable)
+      break
+  print(f"\nMaster\'s hand: {Master}\n{Line}")
+  Cash_player += Pot
