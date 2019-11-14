b46a281444c389bf45164ed9bebf9f9f633212f8
Raaadvis <57595072+Raaadvis@users.noreply.github.com>
2019-11-14 11:51:24 +0200

    Update Poker.py

--- Poker.py
+++ Poker.py
@@ -6,9 +6,6 @@ deck = ["2♦", "3♦", "4♦", "5♦", "6♦", "7♦", "8♦", "9♦", "10♦",
 random.shuffle(deck)
 
 #Variables
-Cash_player = 10000
-Cash_master = 10000
-Pot = 0
 betamount = 0
 callamount = 0
 myhand = []
@@ -17,32 +14,32 @@ Flop = []
 Turn = []
 River = []
 Newline = "\n"
-Star = "*"
-Line = "_"
+Star = "*"*50
+Line = "_"*50
 Space = " "
 
 print("Texas hold \'em poker - Heads up challenge\n")
 name = input("Player's name? ")
 
 #Inserting cards to variables
-for hands in range(2):
-  myhand.append(deck.pop())
-  Master.append(deck.pop())
-for flopcards in range(3):
-  Flop.append(deck.pop())
-for turnrivercards in range(1):
+for cards1 in range(1):
   Turn.append(deck.pop())
   River.append(deck.pop())
+for cards2 in range(2):
+  myhand.append(deck.pop())
+  Master.append(deck.pop())
+for cards3 in range(3):
+  Flop.append(deck.pop())
 
 #Gameplay formula
 def poker_game(Cash_player = 10000, Cash_master = 10000, Pot = 0):
 
-  Table1 = (f"{Newline*10}{Line*50}\nMaster's cash: {Cash_master}$\n\n\nPot: {Pot}{Space*10}")
-  Table2 = (f"\n\n\n{name}\'s hand: {myhand} \nCash: {Cash_player}$\n{Star*50}\n")
+  Table1 = (f"{Newline*10}{Line}\nMaster's cash: {Cash_master}$\n\n\nPot: {Pot}{Space*10}")
+  Table2 = (f"\n\n\n{name}\'s hand: {myhand} \nCash: {Cash_player}$\n{Star}\n")
   BasicTable = (f"{Table1}{Table2}")
   FlopTable = (f"{Table1}{Flop}{Table2}")
-  TurnTable = (f"{Table1}{Flop}  {Turn}{Table2}")
-  RiverTable = (f"{Table1}{Flop}  {Turn}  {River}{Table2}")
+  TurnTable = (f"{Table1}{Flop} {Turn}{Table2}")
+  RiverTable = (f"{Table1}{Flop} {Turn} {River}{Table2}")
 
   Gamestep = BasicTable, FlopTable, TurnTable, RiverTable
   for gamephase in range(len(Gamestep)):
@@ -51,21 +48,24 @@ def poker_game(Cash_player = 10000, Cash_master = 10000, Pot = 0):
     while Choice != "Check" and Choice != "Bet" and Choice != "Fold":
       Choice = input("Incorrect entry, try again: ")
     if Choice == "Check":
-      print(Line*50)
+      print(Line)
       continue
     elif Choice == "Bet":
+      global betamount
       betamount = int(input("Amount - "))
-      print(Line*50)
+      print(Line)
       while betamount > Cash_player:
+        betamount = int(input("Incorrect amount, try again: "))
         if betamount == 0:
           continue
-        betamount = int(input("Incorrect amount, try again: "))
       Cash_player = Cash_player - betamount
       Cash_master = Cash_master - betamount
       Pot = betamount + betamount
+      print(Cash_player)
+      print(Pot)
       continue
-    elif Choice == "Fold":
-      print(RiverTable)
+    elif Choice == "Fold" or gamephase == 3:
+      print(Line, RiverTable)
       break
-  print(f"Master\'s hand: {Master}")
+  print(f"\nMaster\'s hand: {Master}\n{Line}")
 poker_game()
